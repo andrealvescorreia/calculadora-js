96663b1fb12807c2d3f2f373c63cf0db8717e39f
André Alves Correia <andre2013aguabranca@gmail.com>
2022-01-22 11:36:39 -0300

    html 1.2 + js 0.1

---
+++ script.js
@@ -0,0 +1,15 @@
+class Calculadora{
+    
+}
+
+
+const botoesNumeros = document.querySelectorAll('[data-numero]')
+const botoesOperacoes =  document.querySelectorAll('[data-operacao]')
+const botaoIgual = document.querySelector('[data-igual]')
+const botaoLimpar = document.querySelectorAll('[data-limpar]')
+
+const operadorAnteriorTextElement = document.querySelector('[data-operador-anterior]')
+const operadorAtualTextElement = document.querySelector('[data-operador-atual]')
+
+/* dica aleatoria vs code: Alt+z ativa/desativa quebra de linhas.*/
+
